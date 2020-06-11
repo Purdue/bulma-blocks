62c6213b3151bb02416aeb7954a2c87d2b2ec988
jyoung295 <young295@purdue.edu>
2020-06-11 11:28:38 -0400

    remove wp custom category hook

--- src/init.php
+++ src/init.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Blocks Initializer
  *
@@ -9,8 +10,8 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+  exit;
 }
 
 /**
@@ -27,90 +28,78 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
-function bulma_blocks_cgb_block_assets() { // phpcs:ignore
-	// Register block styles for both frontend + backend.
-	wp_register_style(
-		'bulma_blocks-cgb-style-css', // Handle.
-		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-editor' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
-	);
+function bulma_blocks_cgb_block_assets()
+{ // phpcs:ignore
+  // Register block styles for both frontend + backend.
+  wp_register_style(
+    'bulma_blocks-cgb-style-css', // Handle.
+    plugins_url('dist/blocks.style.build.css', dirname(__FILE__)), // Block style CSS.
+    array('wp-editor'), // Dependency to include the CSS after it.
+    null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+  );
 
-	// Register block editor script for backend.
-	wp_register_script(
-		'bulma_blocks-cgb-block-js', // Handle.
-		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
-		true // Enqueue the script in the footer.
-	);
+  // Register block editor script for backend.
+  wp_register_script(
+    'bulma_blocks-cgb-block-js', // Handle.
+    plugins_url('/dist/blocks.build.js', dirname(__FILE__)), // Block.build.js: We register the block here. Built with Webpack.
+    array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'), // Dependencies, defined above.
+    null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+    true // Enqueue the script in the footer.
+  );
 
-	// Register block editor styles for backend.
-	wp_register_style(
-		'bulma_blocks-cgb-block-editor-css', // Handle.
-		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
-	);
+  // Register block editor styles for backend.
+  wp_register_style(
+    'bulma_blocks-cgb-block-editor-css', // Handle.
+    plugins_url('dist/blocks.editor.build.css', dirname(__FILE__)), // Block editor CSS.
+    array('wp-edit-blocks'), // Dependency to include the CSS after it.
+    null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
+  );
 
-	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
-	wp_localize_script(
-		'bulma_blocks-cgb-block-js',
-		'cgbGlobal', // Array containing dynamic data for a JS Global.
-		[
-			'pluginDirPath' => plugin_dir_path( __DIR__ ),
-			'pluginDirUrl'  => plugin_dir_url( __DIR__ ),
-			// Add more data here that you want to access from `cgbGlobal` object.
-		]
-	);
+  // WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
+  wp_localize_script(
+    'bulma_blocks-cgb-block-js',
+    'cgbGlobal', // Array containing dynamic data for a JS Global.
+    [
+      'pluginDirPath' => plugin_dir_path(__DIR__),
+      'pluginDirUrl'  => plugin_dir_url(__DIR__),
+      // Add more data here that you want to access from `cgbGlobal` object.
+    ]
+  );
 
-	/**
-	 * Register Gutenberg block on server-side.
-	 *
-	 * Register the block on server-side to ensure that the block
-	 * scripts and styles for both frontend and backend are
-	 * enqueued when the editor loads.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
-	 * @since 1.16.0
-	 */
-	register_block_type(
-		'cgb/block-bulma-blocks', array(
-			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'bulma_blocks-cgb-style-css',
-			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'bulma_blocks-cgb-block-js',
-			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'bulma_blocks-cgb-block-editor-css',
-		)
-	);
+  /**
+   * Register Gutenberg block on server-side.
+   *
+   * Register the block on server-side to ensure that the block
+   * scripts and styles for both frontend and backend are
+   * enqueued when the editor loads.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
+   * @since 1.16.0
+   */
+  register_block_type(
+    'cgb/block-bulma-blocks',
+    array(
+      // Enqueue blocks.style.build.css on both frontend & backend.
+      'style'         => 'bulma_blocks-cgb-style-css',
+      // Enqueue blocks.build.js in the editor only.
+      'editor_script' => 'bulma_blocks-cgb-block-js',
+      // Enqueue blocks.editor.build.css in the editor only.
+      'editor_style'  => 'bulma_blocks-cgb-block-editor-css',
+    )
+  );
 }
-
-function my_plugin_block_categories( $categories, $post ) {
-	return array_merge(
-			array(
-					array(
-							'slug' => 'bulma-blocks',
-              'title' => __( 'Bulma Blocks', 'bulma-blocks' )
-					),
-        ),
-        $categories
-	);
-}
-add_filter( 'block_categories', 'my_plugin_block_categories', 1, 2 );
 
 // add the bulma container class around all content
-function add_content_container ( $content ) {
-	error_log($content);
-	if ( is_page() || is_single() ) {
-			return (
-				'<div class="content">'.
-					$content.
-				'</div>'
-			);
-	}
+function add_content_container($content)
+{
+  error_log($content);
+  if (is_page() || is_single()) {
+    return ('<div class="content">' .
+      $content .
+      '</div>');
+  }
 }
-add_filter( 'the_content', 'add_content_container');
+add_filter('the_content', 'add_content_container');
 
 // Hook: Block assets.
-add_action( 'init', 'bulma_blocks_cgb_block_assets' );
+add_action('init', 'bulma_blocks_cgb_block_assets');
