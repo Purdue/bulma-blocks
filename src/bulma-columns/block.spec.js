fb731bd61061f0a1c8ab74af5b40ad136cbf16c9
Jesse Young <young295@purdue.edu>
2020-09-16 15:02:23 -0400

    write add/remove columns tests

--- src/bulma-columns/block.spec.js
+++ src/bulma-columns/block.spec.js
@@ -10,6 +10,7 @@ import {
     clickElementByText,
     blockStartup,
     clickCheckbox,
+    updateRangeInput
 } from '../test-helpers'
 
 const block = {blockTitle: 'Bulma - Columns', blockName: 'bulma-blocks/columns'}
@@ -29,4 +30,41 @@ describe( `🔬 ${block.blockTitle} Block`, () => {
         expect(await page.$(`[data-type="${block.blockName}"]`)).not.toBeNull()
         expect( await getEditedPostContent() ).toMatchSnapshot()
     } )
+
+    describe( '🔬 Adding/Removing Columns', () => {
+
+        // test settings to do with adding and removing the inner navigation links
+        test( '🔎 Add Columns.', async () => {
+            await blockStartup(block)
+    
+            const addColumns = 3
+            await updateRangeInput(`.components-input-control__input[aria-label="Number of Columns"]`, addColumns)
+    
+            const columnsNum = await page.$$eval(`[data-type="bulma-blocks/column"]`, array => array.length)
+    
+            expect( columnsNum ).toEqual(addColumns)
+            expect( await getEditedPostContent() ).toMatchSnapshot()
+    
+        })
+    
+        test( '🔎 Add then remove Columns.', async () => {
+            await blockStartup(block)
+    
+            let update = 5
+            await updateRangeInput(`.components-input-control__input[aria-label="Number of Columns"]`, update)
+            
+            let columnsNum = await page.$$eval(`[data-type="bulma-blocks/column"]`, array => array.length)
+            expect( columnsNum ).toEqual(update)
+    
+            update = 2
+            await updateRangeInput(`.components-input-control__input[aria-label="Number of Columns"]`, update)
+    
+    
+            columnsNum = await page.$$eval(`[data-type="bulma-blocks/column"]`, array => array.length)
+            expect( columnsNum ).toEqual(update)
+    
+            expect( await getEditedPostContent() ).toMatchSnapshot()
+    
+        })
+    })
 })
