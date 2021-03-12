ef2206f2d64954a790f4ff5c6234b25a4488f715
Chris Carrel <cjcarrel@purdue.edu>
2021-03-12 09:55:14 -0500

    Version bump 1.4.6.1

--- bulma-blocks.php
+++ bulma-blocks.php
@@ -3,9 +3,9 @@
  * Plugin Name: Bulma Blocks
  * Plugin URI: https://github.com/Purdue/bulma-blocks
  * Description: Bulma Blocks is a Gutenberg Block plugin allowing users to create Bulma elements as blocks.
- * Author: Purdue Marketing and Media
- * Author URI: https://brand.purdue.edu/digital/
- * Version: 1.4.6
+ * Author: Purdue Marketing and Communications
+ * Author URI: https://marcom.purdue.edu/
+ * Version: 1.4.6.1
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  *
